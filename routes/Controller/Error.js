8f43dd18c14daca95ba9242ba38869c9a1090f7b
berkanburuk <berkanburuk.se@gmail.com>
2018-12-16 00:40:44 +0300

    Redirections

--- routes/Controller/Error.js
+++ routes/Controller/Error.js
@@ -1,4 +1,13 @@
+let path = require('path');
+
 module.exports = function(app){
+
+    app.get('/noAuthority', function (request, response) {
+        console.log('Error');
+            response.sendFile(path.resolve('public/Pages/Authority.html'));
+    })
+
+    /*
     app.get('*', function(req, res) {
         console.log('Error');
         var err = ('404');
@@ -6,5 +15,6 @@ module.exports = function(app){
         res.render('error');
         next(err);
     });
+*/
 
 }
