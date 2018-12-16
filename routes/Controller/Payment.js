33a3556bb68e92f5b09aceb19ac87c9f6143066c
berkanburuk <berkanburuk.se@gmail.com>
2018-12-16 21:19:26 +0300

    Controller Session Fixed

--- routes/Controller/Payment.js
+++ routes/Controller/Payment.js
@@ -11,6 +11,29 @@ let mPayment = db.model(dbNames.payment);
 
 let checkUsersRole = require('./RoleCheck');
 
+
+function getAllPaymentByTableId(id){
+
+        return new Promise((resolve, reject) => {
+            mPayment.findAll({
+                where: {
+                    name: data.menuName
+                },
+                include: [
+                    {
+                        model:mFood,
+                        through: mMenuFood
+                    }
+                ]
+            }).then(result=>{
+                resolve(JSON.stringify(result));
+            }).catch(error=>{
+                reject(JSON.stringify(error));
+            })
+    })
+}
+
+
 exports.isExists = function(id) {
     return new Promise((resolve, reject) => {
         mPayment.findOne({
@@ -33,3 +56,35 @@ exports.isExists = function(id) {
 
     })
 }
+
+
+
+module.exports = function (app) {
+
+
+        app.get('/api/order/:getPaymentOfTable', function (request, response) {
+            if (request.session != undefined  && (checkUsersRole.isMatre(request.session.roleId)
+                || checkUsersRole.isAdmin(request.session.roleId) || checkUsersRole.isWaiter(request.session.roleId)))
+            {
+                var tableId = request.params.getPaymentOfTable;
+
+                tableId = parseInt(tableId);
+                getPaymentOfTable(tableId).then(beverage => {
+                    response.end(beverage.toString());
+                }).catch(error => {
+                    response.end(error.toString());
+                })
+            }
+            else {
+                response.write(checkUsersRole.errorMesage(), () => {
+                    response.statusCode = 404;
+                    response.end();
+                })
+            }
+        })
+
+
+}
+
+
+
